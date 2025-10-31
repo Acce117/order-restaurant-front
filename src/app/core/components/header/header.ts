6dc0058ca35a4caa2ad471bccb1c38d37b0d8ae7
Acce117 <e.carralero117@gmail.com>
2025-10-31 12:40:18 -0400

    feat - header component

---
+++ src/app/core/components/header/header.ts
@@ -0,0 +1,27 @@
+import { Component, inject, output } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterModule } from "@angular/router";
+import { MatMenuModule } from "@angular/material/menu";
+import { AppStore } from "../../stores/app.store";
+import { AuthService } from "../../../auth/services/auth-service";
+
+@Component({
+    selector: 'header[appHeader]',
+    templateUrl: './header.html',
+    styleUrl: './header.scss',
+    imports: [MatButtonModule, RouterModule, MatMenuModule]
+})
+export class Header {
+    appStore = inject(AppStore);
+    toggleDrawer = output();
+
+    authService = inject(AuthService);
+
+    toggleTheme() {
+        this.appStore.toggleTheme();
+    }
+
+    logout() {
+        this.authService.logout();
+    }
+}
