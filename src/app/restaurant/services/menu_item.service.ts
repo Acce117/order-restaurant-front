b44a892883b9570f3af9b34f8a688dd744175126
Acce117 <e.carralero117@gmail.com>
2025-11-02 12:31:44 -0500

    feat - menu item change active status

--- src/app/restaurant/services/menu_item.service.ts
+++ src/app/restaurant/services/menu_item.service.ts
@@ -2,6 +2,7 @@ import { EventEmitter, Injectable } from "@angular/core";
 import { Service } from "../../core/services/service";
 import { Observable, tap } from "rxjs";
 import { MenuItem } from "../entities/menu_item";
+import { environment } from "../../../environments/environment";
 
 @Injectable({ providedIn: 'root'})
 export class MenuItemService extends Service<MenuItem> {
@@ -13,14 +14,17 @@ export class MenuItemService extends Service<MenuItem> {
     }
     
     public override create(data: MenuItem): Observable<MenuItem> {
-        return super.create(data).pipe(
-            tap(() => this.updatedData.emit())
-        );
+        return super.create(data)
+            .pipe(tap(() => this.updatedData.emit()));
     }
 
     public override update(id: number, data: MenuItem): Observable<MenuItem> {
-        return super.update(id, data).pipe(
-            tap(() => this.updatedData.emit())
-        );
+        return super.update(id, data)
+            .pipe(tap(() => this.updatedData.emit()));
+    }
+
+    public changeActiveStatus(id: number) {
+        return this.http.patch(`${environment.API_PATH}/${this.modulePath}/change-active-status/${id}`, {})
+            .subscribe(()=>this.updatedData.emit());
     }
 }
