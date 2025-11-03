615da1b269f92d059040b8a4a7a1ece9d517af07
Acce117 <e.carralero117@gmail.com>
2025-11-03 12:17:11 -0500

    fix - sent token only it there is token

--- src/app/auth/interceptors/auth-jwt.interceptor.ts
+++ src/app/auth/interceptors/auth-jwt.interceptor.ts
@@ -7,9 +7,10 @@ import { AuthUserStore } from "../../core/stores/auth_user.store";
 export function authJwtInterceptor(req: HttpRequest<any>, next: HttpHandlerFn): Observable<HttpEvent<any>> {
     const authUserStore = inject(AuthUserStore);
 
-    req = req.clone({
-        headers: req.headers.set('Authorization', `Bearer ${authUserStore.getToken()}`)
-    });
+    if (authUserStore.getToken())
+        req = req.clone({
+            headers: req.headers.set('Authorization', `Bearer ${authUserStore.getToken()}`)
+        });
 
     return next(req);
 }
