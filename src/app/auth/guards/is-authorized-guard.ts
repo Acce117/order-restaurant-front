5cf7de5a7cb143d8730718db6ce769a02b0b6f32
Acce117 <e.carralero117@gmail.com>
2025-11-05 20:43:42 -0500

    feat - redirect to objective page after login

--- src/app/auth/guards/is-authorized-guard.ts
+++ src/app/auth/guards/is-authorized-guard.ts
@@ -1,11 +1,15 @@
 import { inject } from "@angular/core";
 import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from "@angular/router";
 import { AuthUserStore } from "../../core/stores/auth_user.store";
+import { AppStore } from "../../core/stores/app.store";
 
 export function isAuthorizedGuard(roles: string[]): CanActivateFn {
     return (route: ActivatedRouteSnapshot, state: RouterStateSnapshot) => {
         const router = inject(Router);
         const authUserStore = inject(AuthUserStore);
+        const appStore = inject(AppStore);
+
+        appStore.previousUrl = state.url;
 
         const isAuthorized = roles.find((role) => {
             return authUserStore.isAuthorized(role);
--- src/app/auth/guards/valid-token-guard.ts
+++ src/app/auth/guards/valid-token-guard.ts
@@ -3,12 +3,16 @@ import { ActivatedRouteSnapshot, GuardResult, MaybeAsync, Router, RouterStateSna
 import { map } from "rxjs";
 import { AuthUserStore } from "../../core/stores/auth_user.store";
 import { AuthService } from "../services/auth-service";
+import { AppStore } from "../../core/stores/app.store";
 
 export function validTokenGuard (route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {
     const router = inject(Router);
     const authUserStore = inject(AuthUserStore);
     const authService = inject(AuthService);
+    const appStore = inject(AppStore);
 
+    appStore.previousUrl = state.url;
+    
     return authService.isValidJwt(authUserStore.token as string)
         .pipe(
             map((res) => !res ? router.parseUrl('/auth') : true)
