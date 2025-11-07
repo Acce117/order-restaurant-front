eb9c0488ddc9a1699d96ac1f8c1754723e36f2c6
Acce117 <e.carralero117@gmail.com>
2025-11-07 12:22:24 -0500

    style -catalogue

--- src/app/layouts/home/home.ts
+++ src/app/layouts/home/home.ts
@@ -3,10 +3,12 @@ import { RouterOutlet, RouterLinkWithHref } from "@angular/router";
 import { MatSidenavModule } from "@angular/material/sidenav"
 import { Header } from "../../core/components/header/header";
 import { MatMenuItem } from "@angular/material/menu";
+import { MatListModule } from "@angular/material/list";
 @Component({
     selector: 'home',
     templateUrl: './home.html',
-    imports: [RouterOutlet, MatSidenavModule, Header, RouterLinkWithHref, MatMenuItem]
+    imports: [RouterOutlet, MatSidenavModule, Header, RouterLinkWithHref, MatMenuItem, MatListModule],
+    styleUrl: './home.scss',
 })
 export class Home {
 
