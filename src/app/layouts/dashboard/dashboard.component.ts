b2ae291c8594e7f2c87bb030357b7f33ed2258b2
Acce117 <e.carralero117@gmail.com>
2025-11-06 20:05:39 -0500

    feat - responsive drawer

--- src/app/layouts/dashboard/dashboard.component.ts
+++ src/app/layouts/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, computed, signal } from "@angular/core";
 import { Header } from "../../core/components/header/header";
 import { RouterLinkWithHref, RouterModule } from "@angular/router";
 import { MatButtonModule } from "@angular/material/button";
@@ -12,5 +12,14 @@ import { MatMenuItem } from "@angular/material/menu";
     imports: [Header, MatSidenavModule, AppMenu, RouterModule, MatButtonModule, MatMenuItem]
 })
 export class Dashboard {
+    innerWidth = signal(window.innerWidth);
+    isOpen = computed(() => this.innerWidth() > 1024);
+    drawerMode = computed(() => this.innerWidth() > 1024 ? 'side' : 'over');
+    
+    constructor() {
+        window.onresize = () => {
+            this.innerWidth.set(window.innerWidth);
+        };
+    }
 
 }
