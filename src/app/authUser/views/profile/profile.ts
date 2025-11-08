a456c3a0a7d0504bab05d49de811c181e9a7e218
Acce117 <e.carralero117@gmail.com>
2025-11-07 22:17:02 -0500

    feat - profile icon and some style fixes

--- src/app/authUser/views/profile/profile.ts
+++ src/app/authUser/views/profile/profile.ts
@@ -2,11 +2,11 @@ import { Component, inject } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatInputModule } from "@angular/material/input";
 import { AuthUserStore } from "../../../core/stores/auth_user.store";
-import { NgOptimizedImage } from "@angular/common";
 
 @Component({
     selector: 'user-profile',
     templateUrl: './profile.html',
+    styleUrl: './profile.scss',
     imports: [MatInputModule, MatButtonModule],
 })
 export class UserProfile {
